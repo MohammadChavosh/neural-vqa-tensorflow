da06849b9ff30bb79b47adde7082c39a170b809d
Seyed Mohammad Chavoshian <mohammad.chavosh@gmail.com>
2017-07-26 16:24:04 +0430

    add something for test

--- test.py
+++ test.py
@@ -15,27 +15,27 @@ def get_vgg_graph():
 
 	images = tf.placeholder("float", [None, 224, 224, 3])
 	tf.import_graph_def(graph_def, input_map={"images": images})
+	graph = tf.get_default_graph()
 
-	return tf.get_default_graph(), images
+	return graph, tf.Session(graph=graph), images
 
 
-def load_image_features(img_path, graph, images):
+def load_image_features(img_path, graph, sess, images):
 
-	with tf.Session( graph = graph ) as sess:
-		image_batch = np.ndarray((1, 224, 224, 3))
-		image_batch[0, :, :, :] = utils.load_image_array(img_path)
+	image_batch = np.ndarray((1, 224, 224, 3))
+	image_batch[0, :, :, :] = utils.load_image_array(img_path)
 
-		feed_dict = {images: image_batch[0:1, :, :, :]}
-		fc7_tensor = graph.get_tensor_by_name("import/Relu_1:0")
-		fc7_batch = sess.run(fc7_tensor, feed_dict=feed_dict)
-		return fc7_batch[0, :]
+	feed_dict = {images: image_batch[0:1, :, :, :]}
+	fc7_tensor = graph.get_tensor_by_name("import/Relu_1:0")
+	fc7_batch = sess.run(fc7_tensor, feed_dict=feed_dict)
+	return fc7_batch[0, :]
 
 
 def main():
-	graph, images = get_vgg_graph()
-	print load_image_features('Data/train2014/COCO_train2014_000000465294.jpg', graph, images)
-	print load_image_features('Data/train2014/COCO_train2014_000000465294.jpg', graph, images)
-	print load_image_features('Data/train2014/COCO_train2014_000000465294.jpg', graph, images)
+	graph, graph_sess, images = get_vgg_graph()
+	print load_image_features('Data/train2014/COCO_train2014_000000465294.jpg', graph, graph_sess, images)
+	print load_image_features('Data/train2014/COCO_train2014_000000465294.jpg', graph, graph_sess, images)
+	print load_image_features('Data/train2014/COCO_train2014_000000465294.jpg', graph, graph_sess, images)
 
 	g1 = tf.Graph()
 	with g1.as_default() as g:
@@ -44,10 +44,10 @@ def main():
 			matrix2 = tf.constant([[2.],[2.]])
 			product = tf.matmul(matrix1, matrix2)
 
-	with tf.Session( graph = g1 ) as sess:
+	with tf.Session(graph=g1) as sess:
 		print sess.run(product)
 
-	print load_image_features('Data/train2014/COCO_train2014_000000465294.jpg', graph, images)
+	print load_image_features('Data/train2014/COCO_train2014_000000465294.jpg', graph, graph_sess, images)
 
 
 if __name__ == '__main__':
