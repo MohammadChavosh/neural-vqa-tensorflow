df7327b069b6b461d5091dceb58cb024ccace687
Seyed Mohammad Chavoshian <mohammad.chavosh@gmail.com>
2017-12-26 15:43:05 +0330

    test

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ m_feed = np.array([[val, 0, val, 0, val],
           [val, 0, val, 0, 0]])
 
 # tmp_indices = tf.where(tf.equal(tf.less(2, m), True))
-result = m + tf.expand_dims(tf.maximum(2 - tf.reduce_max(m, axis=1), 0), 1)
+result = m + tf.expand_dims(tf.maximum(2 - tf.reduce_max(m, axis=0), 0), 0)
 
 with tf.Session() as sess:
     print(sess.run([result], feed_dict={m: m_feed}))  # [2, 0, 1]
