a29cd2cd40c019ec12892655a253b69aeb09a7af
Seyed Mohammad Chavoshian <mohammad.chavosh@gmail.com>
2017-12-26 15:22:14 +0330

    fix problem

--- test.py
+++ test.py
@@ -4,13 +4,11 @@ import numpy as np
 val = 3
 m = tf.placeholder(tf.int32)
 m_feed = np.array([[val, 0, val, 0, val],
-          [val, 0, 0, 0, val],
+          [0, 0, 0, 0, 0],
           [val, 0, val, 0, 0]])
 
-print np.shape(m_feed)
-print np.shape(m_feed)[1] - np.argmax(m_feed[:, ::-1], axis=1) - 1
 # tmp_indices = tf.where(tf.equal(tf.less(2, m), True))
-# result = tf.reduce_max(tmp_indices)
-#
-# with tf.Session() as sess:
-#     print(sess.run([result, tmp_indices], feed_dict={m: m_feed}))  # [2, 0, 1]
+result = tf.expand_dims(tf.reduce_max(m, axis=1) + 1, 1) + m
+
+with tf.Session() as sess:
+    print(sess.run([result], feed_dict={m: m_feed}))  # [2, 0, 1]
