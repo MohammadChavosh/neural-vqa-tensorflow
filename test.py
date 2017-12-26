f766893ec5e7709d90d19c10182c77960dc010eb
Seyed Mohammad Chavoshian <mohammad.chavosh@gmail.com>
2017-12-26 15:54:42 +0330

    test

--- test.py
+++ test.py
@@ -8,7 +8,9 @@ m_feed = np.array([[val, 0, val, 0, val],
           [val, 0, val, 0, 0]])
 
 # tmp_indices = tf.where(tf.equal(tf.less(2, m), True))
-result = m + tf.expand_dims(tf.maximum(2 - tf.reduce_max(m, axis=0), 0), 0)
+result = m + tf.expand_dims(tf.maximum(2 - tf.reduce_max(m, axis=1), 0), 1)
+tmp_indices = tf.where(tf.equal(tf.less(1, result), True))
+number_prediction = tf.segment_max(tmp_indices[:, 1], tmp_indices[:, 0])
 
 with tf.Session() as sess:
-    print(sess.run([result], feed_dict={m: m_feed}))  # [2, 0, 1]
+    print(sess.run([result, number_prediction], feed_dict={m: m_feed}))  # [2, 0, 1]
